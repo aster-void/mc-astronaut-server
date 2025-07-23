74b0b0551cbaf00124a4f054d04d344cd535d954
aster <137767097+aster-void@users.noreply.github.com>
2025-07-24 00:24:57 +0900

    add openjdk and git

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A Minecraft Server";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          openjdk
+          git
+        ];
+      };
+    });
+}
