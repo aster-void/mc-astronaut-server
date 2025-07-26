b6a61ede2b0352244563812fe8cc8004dfff0714
aster <137767097+aster-void@users.noreply.github.com>
2025-07-26 23:46:21 +0900

    update run.sh: allow running it from any cwd

--- run.sh
+++ run.sh
@@ -3,4 +3,7 @@
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
+
+cd "$(dirname -- "$0")" || exit 1
+
 java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.4.4/unix_args.txt "$@"
